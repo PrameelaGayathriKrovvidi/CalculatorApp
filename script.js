9e0ddb6c544ff00d92ac88a96decbcbcb0ad8dfe
PrameelaGayathriKrovvidi <pgayathrik2002@gmail.com>
2025-05-20 20:10:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Javscript
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click" , () => {
+        const value = btn.textContent;
+
+        if(value === "C") {
+            currentInput = "";
+            display.value = "";
+        } else if(value === "=") {
+            try {
+                currentInput = eval(currentInput).toString();
+                display.value = currentInput;
+            } catch(err) {
+                display.value ="Error";
+                currentInput = "";
+            }
+        } else {
+            currentInput += value;
+            display.value=currentInput;
+        }
+    });
+});
